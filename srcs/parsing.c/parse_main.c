8030f73b56d4c0a8d2bfd65e3bef1c876910f67f
Anna Danylevych <annadanylevych1@gmail.com>
2024-05-12 19:02:45 +0200

    started parsing

--- srcs/parsing.c/parse_main.c
+++ srcs/parsing.c/parse_main.c
@@ -1,41 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.h                                            :+:      :+:    :+:   */
+/*   parse_main.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adanylev <adanylev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/07 11:34:53 by pedro-go          #+#    #+#             */
-/*   Updated: 2024/05/12 19:00:37 by adanylev         ###   ########.fr       */
+/*   Created: 2024/05/12 17:15:02 by adanylev          #+#    #+#             */
+/*   Updated: 2024/05/12 19:02:25 by adanylev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUB3D_H
-# define CUB3D_H
+#include "cub3d.h"
 
-# include "libft.h"
-# include "mlx.h"
-# include <math.h>
-# include <string.h>
-
-typedef struct s_point
+t_parse	parsing_all(int argc, char **argv)
 {
-	int			x;
-	int			y;
-}				t_point;
+	int		fd;
+	t_parse	info;
 
-
-typedef struct s_parse
-{
-	char		**map;
-	char		*no;
-	char		*so;
-	char		*we;
-	char		*ea;
-	int			player;
-	t_point		wall;
-	t_point		person;
-}				t_parse;
-
-
-#endif
+}
